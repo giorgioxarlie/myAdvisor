8448886b835b37427940cc59e09a33548ebfa58e
Giorgio Grassini <giorgiograssini@gmail.com>
2018-02-12 20:27:23 +0100

    Update

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,8 @@ router.get('/onlyme', onlyMe, function(req, res, next) {
   res.render('private');
 });
 
-router.get('/place',(req,res)=>{
-  res.render('place',{places});
+router.get('/map',(req,res)=>{
+  res.render('map',{places});
 })
 
 module.exports = router;
